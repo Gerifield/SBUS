e5404dd48a847a4563167ec5b803a4012539df6e
Gerifield <gergely.radics1@ibm.com>
2020-09-05 23:35:11 +0200

    feat: fix timer for arduino pro micro thx to froqstar

--- SBUS.cpp
+++ SBUS.cpp
@@ -8,13 +8,13 @@ void SBUS::begin() {
 void SBUS::begin(bool useTimer) {
 	if (useTimer) {
 		noInterrupts();
-		TCCR2A  = 0;
-		TCCR2B  = 0;
-		TCNT2   = 0;
-		OCR2A   = 249;
-		TCCR2A |= (1 << WGM21);
-		TCCR2B |= (1 << CS22);
-		TIMSK2 |= (1 << OCIE2A);
+    TCCR0A  = 0;
+    TCCR0B  = 0;
+    TCNT0   = 0;
+    OCR0A   = 249;
+    TCCR0A |= (1 << WGM01);
+    TCCR0B |= (1 << CS00);
+    TIMSK0 |= (1 << OCIE0A);
 		interrupts();
 	}
 
